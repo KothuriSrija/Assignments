1cf3e849195295289f59cb7229639cc7da4604b3
Kothuri Srija <67414863+KothuriSrija@users.noreply.github.com>
2022-03-03 21:51:37 +0530

    Add files via upload

---
+++ Core Java 11/Exceptional Handling/Java 11/src/AssignQ2.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+class AssignQ2 {
+    public static void main(String[] args)
+    {
+          // Generic list using var
+        var<var> al = new ArrayList<>();
+            
+          // add elements
+        al.add(6);
+        al.add(12);
+        al.add(18);
+        
+        // It will print the list
+        System.out.println(al);
+    }
+}
