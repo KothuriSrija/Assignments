d0870a7580365ee78dc8cc15fd6bd7f4d96c908f
Kothuri Srija <67414863+KothuriSrija@users.noreply.github.com>
2022-03-07 00:03:38 +0530

    Add files via upload

---
+++ JUnit/src/main/java/io/junit/lifecycle.java
@@ -0,0 +1,11 @@
+package io.junit;
+
+public class lifecycle {
+	 public static int add(int a, int b){
+	        return a+b;
+	    }
+	    public static int sub(int a, int b){
+	        return a-b;
+	    }
+
+	}
---
+++ JUnit/src/test/java/io/junit/bankaccountTest.java
@@ -0,0 +1,25 @@
+package io.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+class bankaccountTest {
+
+	@Test
+	 void Withdraw() throws InsufficientBalanceException {
+        assertThrows(InsufficientBalanceException.class,new Executable() {
+			public void execute() throws Throwable {
+				bankaccount.withdraw(800);
+			}
+		});
+        assertEquals(500, bankaccount.withdraw(100));
+        System.out.println(bankaccount.withdraw(100));
+    }
+
+}
